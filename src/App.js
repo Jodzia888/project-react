272dd9fbc8763fa79fa42d1f96c23746d78ecbea
Jodzia888 <jkonopka28@wp.pl>
2024-09-17 11:24:03 +0200

    Pierwszy commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import InfoBox from "./components/InfoBox/InfoBox";
+import List from './components/List/List';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <InfoBox />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InfoBox/InfoBox.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const InfoBox = () =>{
+    const name = "Jakub"
+    return <p>Nazywam się {name}!</p>
+}
+
+export default InfoBox
